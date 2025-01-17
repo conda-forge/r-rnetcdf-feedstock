f70e2ad6f631d5254264f0395a0b709fccda2cb3
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-16 20:28:28 -0800

    fix Rscript path

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ export DISABLE_AUTOBREW=1
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
     sed -i "s?\${R_HOME}/bin?${BUILD_PREFIX}/bin?g" tools/make-recursive.sh
-    sed -i "s?\${R_share}/bin?${PREFIX}/lib/R/share?g" tools/make-recursive.sh
-    sed -i "s?\${R_etc}/bin?${PREFIX}/lib/R/etc?g" tools/make-recursive.sh
+    sed -i "s?\${R_share}?${PREFIX}/lib/R/share?g" tools/make-recursive.sh
+    sed -i "s?\${R_etc}?${PREFIX}/lib/R/etc?g" tools/make-recursive.sh
 fi
 ${R} CMD INSTALL --build . ${R_ARGS:-}
